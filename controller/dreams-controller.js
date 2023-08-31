2914d58c89367b947a8096523bc6659d70342ce9
olgastukova <stukova.oa@gmail.com>
2023-08-31 16:41:14 -0400

    delete, edit dream

--- controller/dreams-controller.js
+++ controller/dreams-controller.js
@@ -59,9 +59,71 @@ const add = (req, res) => {
             res.status(500).send("error");
           });
       };
+      const dlt = (req, res) => {
+        const { id } = req.params;
+        if (!id) res.status(400).res.json("Please provide an Id");
+      
+        knex("dreams")
+          .where({ id: id })
+          .del()
+          .then((response) => {
+            if (!response) {
+              return res
+                .status(400)
+                .json({ message: `Item with ID: ${id} to be deleted not found.` });
+            }
+            res.status(200).send("Item deleted");
+          })
+          .catch(() => {
+            res.status(500).json({ message: "Unable to delete item" });
+          });
+      };
+
+      const edit = (req, res) => {
+        const {
+          dream_name,
+          description,
+          category
+        } = req.body;
+        const newDreamInfo = {
+          dream_name,
+          description,
+          category
+        };
+        if (
+          !dream_name
+        ) {
+          return res.status(400).json("Please write your dream name");
+        }
+      
+        knex("dreams")
+          .where({ id: req.params.id })
+          .then((dream) => {
+            if (!dream.length) {
+              res.status(404).json("Dream not found");
+            } else {
+              knex("dreams")
+                .where({ id: req.params.id })
+                .update(newDreamInfo)
+                .then(() => {
+                  return knex("dreams").where({
+                    id: req.params.id,
+                  });
+                })
+                .then((updatedDream) => res.json(updatedDream[0]))
+                .catch(() =>
+                  res
+                    .status(500)
+                    .json(`Unable to update dream with id ${req.params.id}`)
+                );
+            }
+          });
+      };
 module.exports = {
   add,
   getDream,
-  getAll
+  getAll,
+  dlt,
+  edit
 };
 
