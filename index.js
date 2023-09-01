8d2fdfc7d02fca54d59c74dcb7b6edfc68cb443f
olgastukova <stukova.oa@gmail.com>
2023-08-31 23:02:44 -0400

    implement ai bot

--- index.js
+++ index.js
@@ -3,13 +3,36 @@ const express = require ('express');
 const app = express();
 const cors = require('cors');
 const dreams = require("./routes/dreams")
-
+const OpenAI = require ('openai');
 
 app.use(express.json());
 app.use(cors());
 
 app.use("/dreams", dreams)
 
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY 
+});
+
+app.post("/ask", async (req, res) => {
+  const prompt = req.body.prompt;
+
+  try {
+    if (prompt == null) {
+      throw new Error("Uh oh, no prompt was provided");
+    }
+
+    const chatCompletion = await openai.chat.completions.create({
+        model: "gpt-3.5-turbo",
+        messages: [{"role": "user", "content": prompt}],
+      });
+      console.log(chatCompletion.choices[0].message);
+      return res.status(200).json(chatCompletion.choices[0].message)}
+     
+   catch (error) {
+    console.log(error.message);
+  }
+});
 
 app.listen (8080, function () {
     console.log("Running 8080")
